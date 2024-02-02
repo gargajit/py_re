d549a1ca82f209cd04b7e48a810f5cdda830b1c3
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-02-02 23:28:04 +0530

    Update re_validate.py

--- re_validate.py
+++ re_validate.py
@@ -4,4 +4,5 @@ email = input("What's your email? ").strip()
 
 #print ("valid") if re.search(r"^.+@.+\.com$", email) else print("invalid")
 #print ("valid") if re.search(r"^[^@]+@[^@]+\.com$", email) else print("invalid")
-print ("valid") if re.search(r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.(com|edu|in|gov|net)$", email, re.IGNORECASE) else print("invalid")
+#print ("valid") if re.search(r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.(com|edu|in|gov|net)$", email, re.IGNORECASE) else print("invalid")
+print ("valid") if re.search(r"^\w+@\w+\.(com|edu|in|gov|net)$", email, re.IGNORECASE) else print("invalid")

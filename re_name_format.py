5943e95f9d018048d8168947a54eae9c98bc41fe
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-02-03 19:51:44 +0530

    Update re_name_format.py

--- re_name_format.py
+++ re_name_format.py
@@ -1,3 +1,4 @@
+'''
 import re
 
 name = input("Enter name: ").strip()
@@ -5,5 +6,16 @@ matches = re.search(r"^(.+), *(.+)$", name)
 if matches:
     last, first = matches.groups()
     name = f"{first} {last}"
+
+print(f"Hello, {name}")
+'''
+
+
+# Using Walrus (:=) function
+import re
+
+name = input("Enter name: ").strip()
+if matches := re.search(r"^(.+), *(.+)$", name):
+    name = matches.group(2) + " " + matches.group(1)
 
 print(f"Hello, {name}")

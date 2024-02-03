517e34dc43c3cbfdcbe553c232c245f8409950c4
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-02-03 21:17:20 +0530

    Update re_username_from_url.py

--- re_username_from_url.py
+++ re_username_from_url.py
@@ -2,9 +2,21 @@ import re
 
 url = input("URL: ").strip()
 
+'''
 username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
 print(f"Username: {username}")
 
 # The above runs perectly if user enters a twitter URL, but it will print a non-twitter URL
 # URL: www.google.com/abc
 # Output: www.google.com/abc
+'''
+
+matches =  re.search(r"^(?:https?://)?(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
+if matches:
+    print(f"Username:", matches.group(1)) 
+
+# OR using Walrus function
+'''
+if matches :=  re.search(r"^(?:https?://)?(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+    print(f"Username:", matches.group(1)) 
+'''

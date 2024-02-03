298ab27be19664101fb5b8a3947d866e3cd4d9d4
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-02-03 21:27:08 +0530

    Update re_username_from_url.py

--- re_username_from_url.py
+++ re_username_from_url.py
@@ -11,12 +11,27 @@ print(f"Username: {username}")
 # Output: www.google.com/abc
 '''
 
+
+'''
 matches =  re.search(r"^(?:https?://)?(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE)
 if matches:
     print(f"Username:", matches.group(1)) 
+'''
 
 # OR using the Walrus function
 '''
 if matches :=  re.search(r"^(?:https?://)?(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
     print(f"Username:", matches.group(1)) 
+'''
+
+
+# Final one
+matches =  re.search(r"^(?:https?://)?(?:www\.)?twitter\.com/(\w+)", url, re.IGNORECASE)
+if matches:
+    print(f"Username:", matches.group(1)) 
+
+'''
+In the final one we have removed dollar so as to include ? or # after username.
+But it will only work when we are using word character \w i.e. [a-z0-9_]
+Also note: even with \w we are using parenthesis so that re.search returns it
 '''
